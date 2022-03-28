8ba1596666ff1128492dc2dab228f8ecbac35330
MDWADUD <43837530+MDWADUD@users.noreply.github.com>
2022-03-28 19:31:24 -0400

    Update main.py
    Solved issues

--- main.py
+++ main.py
@@ -1,99 +1,70 @@
-from flask import Flask, Response, request
-from s3functions import get_users, addfolder, delete_user, get_user_files, getfilecontent, delete_file, upload_file
+import requests_call as tv_client
+from flask import Flask
+from flask_restful import Api, Resource
+from flask_sqlalchemy import SQLAlchemy
+import datetime
 
 app = Flask(__name__)
+api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
 
+global dict_shows
+global dict_shows_ep
 
+class LogRequests(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    accesstime = db.Column(db.String(100), nullable = False)
+    call = db.Column(db.String(500), nullable = False)
 
-# def get_users():
-#   foldernames = []
-#   objects = s3.list_objects_v2(Bucket=BUCKET_NAME, Delimiter='/', Prefix='')
-#   if(len(objects)==0):
-#     return []
-#   for i in objects["CommonPrefixes"]:
-#     foldernames.append(i["Prefix"])
-#   return foldernames
+db.create_all()
 
-@app.route('/register/<username>')
-def register(username):
-  users = get_users()
-  print(users)
-  if(username+'/' in users):
-    return Response(status=403)
-  addfolder(username)
-  return Response(status=200)
+dict_shows = {}
+dict_shows_ep = {}
 
-@app.route('/remove/<username>')
-def remove(username):
-  users= get_users()
-  if(username+'/' not in users):
-    return Response(status=403)
-  delete_user(username)
-  return Response(status=200)
-
-@app.route('/upload/<id>',methods=["POST"])
-def upload(id):
-  if(request.method=="GET"):
-    return Response(status = 400)
-  #get api key 
-  username = request.headers.get("api-key")
-  file = request.files['file']
-  #check if the user exist
-  users= get_users()
-  if(username+'/' not in users):
-    return Response(status=403)
-  #check if filename is already taken
-  userfiles= get_user_files(username)
-  if("{}/{}".format(username,id) in userfiles):
-    return Response(status=403)
-  #if user exist and filename not taken, upload
-  upload_file(username,file,id)
-  return Response(status=200)
-
-@app.route('/files')
-def files():
-  #get api key 
-  username = request.headers.get("api-key")
-  #check if the user exist
-  users= get_users()
-  if(username+'/' not in users):
-    return Response(status=403)
-  #if user exist: return all of that user's files  
-  return Response(data=get_user_files(username),status=200) 
+def run_server():
+  global dict_shows
+  global dict_shows_ep
+  dict_shows = tv_client.fetchAllShows()
+  dict_shows_ep = tv_client.fetchAllShowsEpisodes()
+  app.run(debug=True,use_reloader=False, port=5000)
     
-
-@app.route('/files/<id>')
-def filesContent(id):
-  #get apikey
-  username = request.headers.get("api-key")
-  # check if user exist
-  users= get_users()
-  if(username+'/' not in users):
-    return Response(status=403)
-  #check if file exist
-  userfiles= get_user_files(username)
-  #if file exist return that
-  if("{}/{}".format(username,id) in userfiles):
-    return Response(data=getfilecontent,status=200)
-  #else return 403 
-  return Response(status=403)
-
-
-@app.route('/delete/<id>')
-def deleteContent(id):
-  #get apikey
-  username = request.headers.get("api-key")
-  # check if user exist
-  users = get_users()
-  if(username+'/' not in users):
-    return Response(status=403)
-  #check if file exist
-  userfiles= get_user_files(username)
-  print(userfiles)
-  if("{}/{}".format(username,id) in userfiles):
-    delete_file(username,id)
-    return Response(status=200)
-  return Response(status=403)
-
-if __name__ == '__main__':
-  app.run(debug=True, host='0.0.0.0')
+class GetShows(Resource):
+  global dict_shows
+  global dict_shows_ep
+  def get(self, month1, month2):
+    print("Calling... /diff/"+month1+"/"+month2)
+        
+    date_time_str = datetime.datetime.now().strftime('%B %d, %Y %H:%M %p')
+    url = "/diff/"+month1+"/"+month2
+        
+    dict_mon1 = tv_client.fetchAllShowsByMonth(month1, dict_shows_ep)
+    dict_mon2 = tv_client.fetchAllShowsByMonth(month2, dict_shows_ep)
+    #print(dict_mon1)
+    #print(dict_mon2)
+    show_id_list = [item for item in dict_mon2['list'] if item not in dict_mon1['list']]
+    show_list = []
+    for show_details in dict_shows.get("list"):
+      if show_details["id"] in show_id_list:
+        show_list.append({"id":show_details['id'],"name":show_details['name'], "url":show_details['url']})
+        count = len(show_id_list)
+        message = "Shows aired in month "+month2+" but not aired in "+month1
+        log_request = LogRequests(accesstime=date_time_str, call=url)
+        db.session.add(log_request)
+        db.session.commit()
+        return {"count": count, "message": message, "show_list": show_list}
+    
+class GetRequestLog(Resource):
+  def get(self):
+    log_requests = LogRequests.query.all()
+    req_list = []
+    for each_rec in log_requests:
+      req_list.append({"datetime": each_rec.accesstime, "call": each_rec.call})
+    return req_list
+    
+api.add_resource(GetShows, "/diff/<string:month1>/<string:month2>")
+api.add_resource(GetRequestLog, "/list")
+        
+if __name__ == "__main__":
+  run_server()
