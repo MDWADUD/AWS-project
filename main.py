0348c96d728846c4b13b89d60a53e457caae63bb
MDWADUD <43837530+MDWADUD@users.noreply.github.com>
2022-03-28 19:55:41 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,46 +1,118 @@
-import sys
-import requests_call as tv_client
-import boto3, json
+from flask import Flask
+from flask import Response
+from flask import request
+import boto3
+app = Flask(__name__)
+
+Bucket_name = "mdawadud"
+# client--low-level AWS access
+client = boto3.client('s3', aws_access_key_id = 'AKIAJHQ7I5KGYVIOUNQQ', aws_secret_access_key ='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')
+# Resource--higher-level access
+Resource = boto3.resource('s3',aws_access_key_id = 'AKIAJHQ7I5KGYVIOUNQQ', aws_secret_access_key ='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')
 
 
-AWS_ACCESS_KEY = 'AKIAJHQ7I5KGYVIOUNQQ'
-AWS_ACCESS_SECRET_KEY = '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'
-S3_BUCKET_NAME = 'mdawadud'
+def uploads(username,file,filename):
+  object = Resource.Object(Bucket_name, f"{username}/{filename}")
+  object.put(Body=file)
+@app.route('/upload/<id>',methods=["POST"])
+def upload(id):
+  if(request.method=="POST"):
+    username = request.headers.get("api-key")
+    file = request.files['file']
+    users= users_name()
+    return Response(status=200)
+  if(username+'/' not in users):
+    return Response(status=403)
+  userfiles= get_user_files(username)
+  if(f"{username}/{id}" in userfiles):
+    return Response(status=403)
+  uploads(username,file,id)
+  return Response(status=200)
 
 
-n = len(sys.argv)
+def users_name():
+  Crear_folder_for_user = []
+  objects = client.list_objects_v2(Bucket=Bucket_name, Delimiter='/', Prefix='')
+  if(len(objects)==0):
+    return []
+  for i in objects["CommonPrefixes"]:
+    Crear_folder_for_user.append(i["Prefix"])
+  return Crear_folder_for_user
+@app.route('/register/<username>')
+def addfolder(folder_name):
+  client.put_object(Bucket=Bucket_name, Key=(folder_name+'/'))
+def register(username):
+  users = users_name()
+  print(users)
+  if(username+'/' in users):
+    return Response(status=403)
+  addfolder(username)
+  return Response(status=200)
 
-if(n==1):
-  print("Please Enter a Month in yyyy-dd Format")
-elif(n>2):
-  print("Please Enter Only One Month in yyyy-dd Format")
-else:
-  month = str(sys.argv[1])
-  print("Processing Excecution of "+sys.argv[0])
-    
-  print("Passed Argument:", month)
-  dict_shows = tv_client.fetchAllShows()
-  dict_shows_ep = tv_client.fetchAllShowsEpisodes()
-    
-  dict_mon1 = tv_client.fetchAllShowsByMonthWithDate(month, dict_shows_ep)
-    
-  if(dict_mon1["status"]==200):      
-    s3 = boto3.resource('s3',
-    region_name='us-east-2',
-    aws_access_key_id=AWS_ACCESS_KEY,
-    aws_secret_access_key=AWS_ACCESS_SECRET_KEY).Bucket(S3_BUCKET_NAME)
-    json.dump_s3 = lambda obj, f: s3.Object(key=f).put(Body=json.dumps(obj))
-        
-    file = None
-    key = ""
-    fetched_rec = [item for item in dict_mon1['list']]
-    show_list = []
-    for show_details in dict_shows.get("list"):
-      for item in fetched_rec:
-        if len(item["date_list"])>0:
-          key = str(len(item['date_list']))+"/"+show_details["name"].replace(" ","_")
-          print("Saving file: "+key)
-          json.dump_s3(item['date_list'], key)
-                        
-    else:
-      print(dict_mon1["message"])
+
+
+
+def delete_user(key):
+  bucket = Resource.Bucket(Bucket_name)
+  bucket.objects.filter(Prefix=f"{key}/").delete()
+@app.route('/remove/<username>')
+def remove(username):
+  users= users_name()
+  if(username+'/' not in users):
+    return Response(status=403)
+  delete_user(username)
+  return Response(status=200)
+
+
+def get_user_files(username):
+  files = []
+  bucket = Resource.Bucket(Bucket_name)
+  for object in bucket.objects.filter(Prefix=username):
+    files.append(object.key)
+  return files
+@app.route('/files')
+def files():
+  username = request.headers.get("api-key")
+  users= users_name()
+  if(username+'/' not in users):
+    return Response(status=403)
+  return Response(data=get_user_files(username),status=200) 
+@app.route('/files/<id>')
+def filesContent(id):
+  username = request.headers.get("api-key")
+  users= users_name()
+  if(username+'/' not in users):
+    return Response(status=403)
+  userfiles= get_user_files(username)
+  if(f"{username}/{id}" in userfiles):
+    return Response(data=filecontent,status=200)
+  #else return 403 
+  return Response(status=403)
+
+def filecontent(username,id):
+  bucket = Resource.Bucket(Bucket_name)
+  file=bucket.objects.filter(Prefix=f"{username}/{id}")
+  for object in file:
+    return object.get()["Body"].read()
+
+
+def delete(username,key):
+  bucket = Resource.Bucket(Bucket_name)
+  bucket.objects.filter(Prefix=f"{username}/{key}").delete()
+@app.route('/delete/<id>')
+def deleteContent(id):
+  username = request.headers.get("api-key")
+  users = users_name()
+  if(username+'/' not in users):
+    return Response(status=403)
+  userfiles = get_user_files(username)
+  print(userfiles)
+  if(f"{username}/{id}") in userfiles:
+    delete(username,id)
+    return Response(status=200)
+  return Response(status=403)
+
+
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', debug=True)
