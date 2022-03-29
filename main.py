e1eb306818e8386c00012d4a360c43c8e305c7b6
MDWADUD <43837530+MDWADUD@users.noreply.github.com>
2022-03-28 20:04:28 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,118 +1,32 @@
-from flask import Flask
-from flask import Response
-from flask import request
-import boto3
-app = Flask(__name__)
+{\rtf1\ansi\ansicpg1252\cocoartf2513
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
 
-Bucket_name = "mdawadud"
-# client--low-level AWS access
-client = boto3.client('s3', aws_access_key_id = 'AKIAJHQ7I5KGYVIOUNQQ', aws_secret_access_key ='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')
-# Resource--higher-level access
-Resource = boto3.resource('s3',aws_access_key_id = 'AKIAJHQ7I5KGYVIOUNQQ', aws_secret_access_key ='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')
-
-
-def uploads(username,file,filename):
-  object = Resource.Object(Bucket_name, f"{username}/{filename}")
-  object.put(Body=file)
-@app.route('/upload/<id>',methods=["POST"])
-def upload(id):
-  if(request.method=="POST"):
-    username = request.headers.get("api-key")
-    file = request.files['file']
-    users= users_name()
-    return Response(status=200)
-  if(username+'/' not in users):
-    return Response(status=403)
-  userfiles= get_user_files(username)
-  if(f"{username}/{id}" in userfiles):
-    return Response(status=403)
-  uploads(username,file,id)
-  return Response(status=200)
-
-
-def users_name():
-  Crear_folder_for_user = []
-  objects = client.list_objects_v2(Bucket=Bucket_name, Delimiter='/', Prefix='')
-  if(len(objects)==0):
-    return []
-  for i in objects["CommonPrefixes"]:
-    Crear_folder_for_user.append(i["Prefix"])
-  return Crear_folder_for_user
-@app.route('/register/<username>')
-def addfolder(folder_name):
-  client.put_object(Bucket=Bucket_name, Key=(folder_name+'/'))
-def register(username):
-  users = users_name()
-  print(users)
-  if(username+'/' in users):
-    return Response(status=403)
-  addfolder(username)
-  return Response(status=200)
-
-
-
-
-def delete_user(key):
-  bucket = Resource.Bucket(Bucket_name)
-  bucket.objects.filter(Prefix=f"{key}/").delete()
-@app.route('/remove/<username>')
-def remove(username):
-  users= users_name()
-  if(username+'/' not in users):
-    return Response(status=403)
-  delete_user(username)
-  return Response(status=200)
-
-
-def get_user_files(username):
-  files = []
-  bucket = Resource.Bucket(Bucket_name)
-  for object in bucket.objects.filter(Prefix=username):
-    files.append(object.key)
-  return files
-@app.route('/files')
-def files():
-  username = request.headers.get("api-key")
-  users= users_name()
-  if(username+'/' not in users):
-    return Response(status=403)
-  return Response(data=get_user_files(username),status=200) 
-@app.route('/files/<id>')
-def filesContent(id):
-  username = request.headers.get("api-key")
-  users= users_name()
-  if(username+'/' not in users):
-    return Response(status=403)
-  userfiles= get_user_files(username)
-  if(f"{username}/{id}" in userfiles):
-    return Response(data=filecontent,status=200)
-  #else return 403 
-  return Response(status=403)
-
-def filecontent(username,id):
-  bucket = Resource.Bucket(Bucket_name)
-  file=bucket.objects.filter(Prefix=f"{username}/{id}")
-  for object in file:
-    return object.get()["Body"].read()
-
-
-def delete(username,key):
-  bucket = Resource.Bucket(Bucket_name)
-  bucket.objects.filter(Prefix=f"{username}/{key}").delete()
-@app.route('/delete/<id>')
-def deleteContent(id):
-  username = request.headers.get("api-key")
-  users = users_name()
-  if(username+'/' not in users):
-    return Response(status=403)
-  userfiles = get_user_files(username)
-  print(userfiles)
-  if(f"{username}/{id}") in userfiles:
-    delete(username,id)
-    return Response(status=200)
-  return Response(status=403)
-
-
-
-if __name__ == '__main__':
-  app.run(host='0.0.0.0', debug=True)
+\f0\fs24 \cf0 import requests\
+#import boto3\
+\
+apikey = "md"\
+\
+# security credentials for folder check in the s3 bucket\
+# Retrieve the list of existing buckets\
+# s3 = boto3.client('s3',aws_access_key_id = 'AKIAJHQ7I5KGYVIOUNQQ', aws_secret_access_key ='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')\
+# response = s3.list_buckets()\
+\
+# Output the bucket names\
+# print('Existing buckets:')\
+# for bucket in response['Buckets']:\
+#   print(f'\{bucket["Name"]\}')\
+\
+\
+\
+# delete = requests.get("https://FinalAWSRedo.mdwadud.repl.co/delete/test.txt",headers=\{"api-key":apikey\})\
+# print(delete.status_code)\
+\
+\
+file = \{'file':open('test.txt','r')\}\
+upload = requests.post("https://FinalAWSRedo.mdwadud.repl.co/upload/asdf.txt",files=file,headers=\{"api-key":apikey\})\
+print(upload.status_code)\
+}
